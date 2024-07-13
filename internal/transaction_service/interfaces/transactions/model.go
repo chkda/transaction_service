2eef915b6c4feae17ab31f242d21bb54d63784d0
Chhaya <chayakudas.das56@gmail.com>
2024-07-13 16:19:24 +0530

    Changing id type

--- internal/transaction_service/interfaces/transactions/model.go
+++ internal/transaction_service/interfaces/transactions/model.go
@@ -1,7 +1,7 @@
 package transactions
 
 type Transaction struct {
-	Amount   string  `json:"amount"`
+	Amount   float64 `json:"amount"`
 	Type     string  `json:"type"`
-	ParentId float64 `json:"parent_id"`
+	ParentId int32   `json:"parent_id"`
 }
