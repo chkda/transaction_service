f5b87cfd72aa48cdc81af9446a518542fb98182a
Chhaya <chayakudas.das56@gmail.com>
2024-07-13 23:13:37 +0530

    Adding service initialisation

--- internal/transaction_service/service.go
+++ internal/transaction_service/service.go
@@ -1,1 +1,70 @@
 package transaction_service
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/chkda/transaction_service/internal/transaction_service/app"
+	"github.com/chkda/transaction_service/internal/transaction_service/interfaces/healthcheck"
+	"github.com/chkda/transaction_service/internal/transaction_service/interfaces/sum"
+	"github.com/chkda/transaction_service/internal/transaction_service/interfaces/transactions/create"
+	"github.com/chkda/transaction_service/internal/transaction_service/interfaces/transactions/read"
+	typ "github.com/chkda/transaction_service/internal/transaction_service/interfaces/types"
+	badgercache "github.com/chkda/transaction_service/pkg/datastores/cache/badger"
+	rediscache "github.com/chkda/transaction_service/pkg/datastores/cache/redis"
+	"github.com/chkda/transaction_service/pkg/datastores/database/mysqlstore"
+	"github.com/chkda/transaction_service/pkg/metrics/prometheus"
+	"github.com/labstack/echo/v4"
+)
+
+type Config struct {
+	HTTPPort         string             `json:"http_port"`
+	RedisConfig      *rediscache.Config `json:"redis"`
+	MySQLConfig      *mysqlstore.Config `json:"mysql"`
+	PrometheusConfig *prometheus.Config `json:"prometheus"`
+}
+
+func Start(configFile string) {
+	file, err := os.Open(configFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	config := &Config{}
+	err = json.Unmarshal(fileBytes, config)
+	if err != nil {
+		panic(err)
+	}
+
+	redisClient := rediscache.New(config.RedisConfig)
+	badgerClient, err := badgercache.New()
+	if err != nil {
+		panic(err)
+	}
+	mysqlClient, err := mysqlstore.New(config.MySQLConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	appHandler := app.New(badgerClient, redisClient, mysqlClient)
+	healthcheckController := healthcheck.New()
+	createTransactionController := create.New(appHandler)
+	readTransactionController := read.New(appHandler)
+	sumController := sum.New(appHandler)
+	typesController := typ.New(appHandler)
+
+	serv := echo.New()
+	serv.GET(healthcheckController.GetRoute(), healthcheckController.Handler)
+	serv.PUT(createTransactionController.GetRoute(), createTransactionController.Handler)
+	serv.GET(readTransactionController.GetRoute(), readTransactionController.Handler)
+	serv.GET(sumController.GetRoute(), sumController.Handler)
+	serv.GET(typesController.GetRoute(), typesController.Handler)
+	serv.Logger.Fatal(serv.Start(":" + config.HTTPPort))
+}
